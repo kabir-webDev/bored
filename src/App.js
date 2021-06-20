a1d201d9f303a0e5176f2d0189d0600a8c6a84fe
Kabir Hasan <kabir.webdev@gmail.com>
2021-06-20 20:09:53 +0600

    Re-Write

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
-import "./App.css";
-import Home from "./components/ReactRouter/Home";
-import PageB from "./components/ReactRouter/PageB";
-import PageA from "./components/ReactRouter/PageA";
-import NotFound from "./components/ReactRouter/NotFound";
-import { Route, Switch } from "react-router-dom";
-import Drawer from "./components/ReactRouter/Drawer";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-  },
-});
-
-export default function App() {
-  const classes = useStyles();
+function App() {
   return (
-    <div className={classes.container}>
-      <Drawer />
-      <Switch>
-        <Route exact from="/" render={(props) => <Home {...props} />} />
-        <Route exact path="/pageB" render={(props) => <PageB {...props} />} />
-        <Route exact path="/pageA" render={(props) => <PageA {...props} />} />
-        <Route from="*" render={(props) => <NotFound {...props} />} />
-      </Switch>
+    <div className="app">
+      <h2>This is app</h2>
     </div>
   );
 }
+
+export default App;
